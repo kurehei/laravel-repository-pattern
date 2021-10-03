a3a1c440023381a644c642d1220133b9e570785b
榑林晃平 <kureykou321@gmail.com>
2021-10-03 16:39:29 +0900

    ルーティング設定

--- api/routes/web.php
+++ api/routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/posts', 'App\Http\Controllers\PostController@index');
+
 Route::get('/', function () {
     return view('welcome');
 });
