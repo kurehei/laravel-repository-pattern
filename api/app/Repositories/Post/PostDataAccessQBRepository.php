1fa681ea31a7127eb532f8a7ea0b3840d5de61a0
榑林晃平 <kureykou321@gmail.com>
2021-10-04 00:06:12 +0900

    Entityにデータを保存するようにした

--- api/app/Repositories/Post/PostDataAccessQBRepository.php
+++ api/app/Repositories/Post/PostDataAccessQBRepository.php
@@ -2,12 +2,15 @@
 
 namespace App\Repositories\Post;
 use DB;
+use App\Entities\Post;
 
 class PostDataAccessQBRepository implements PostDataAccessRepositoryInterface{
 
   protected $table = 'users';
 
   public function getAll() {
-    return DB::table('posts')->get();
+    return DB::table('posts')->get()->map(function($post) {
+      return new Post($post->id, $post->name, $post->detail);
+    });
   }
 }
