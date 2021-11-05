92d16c2b108b9747719a4cf56f7a9059e69b2cdf
榑林晃平 <kureykou321@gmail.com>
2021-11-06 07:10:13 +0900

    テーブルの修正

--- api/database/migrations/2021_11_05_214305_create_posts_table.php
+++ api/database/migrations/2021_11_05_214305_create_posts_table.php
@@ -14,7 +14,7 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name')->nullable(false);
             $table->string('detail');
             $table->timestamps();
