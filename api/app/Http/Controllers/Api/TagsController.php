a45511c430d86c6c6d6b7a2ea24bc4b418f0bdf9
榑林晃平 <kureykou321@gmail.com>
2022-01-06 15:37:33 +0900

    タグの保存する処理をControllerに追加

--- api/app/Http/Controllers/Api/TagsController.php
+++ api/app/Http/Controllers/Api/TagsController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Services\Tags\TagServiceInterface;
 
 class TagsController extends Controller
 {
-    //
+
+    protected $tagService;
+
+    /**
+     *
+     * @param TagServiceInterface $tagServiceInterface
+     */
+    public function __construct(TagServiceInterface $tagServiceInterface) {
+        $this->tagService = $tagServiceInterface;
+    }
 
     public function store(Request $request) {
-
+        return $this->tagService->saveTag($request->tags);
     }
 }
